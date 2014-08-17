b90ecc600a6c62e06d110010afbe8eb58a30aa69
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-08-16 21:47:05 -0700

    Finishes route for getting song lyrics through api

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -5,11 +5,16 @@ end
 
 get '/lyrics' do
   artist = params[:artist].split().join("%20")
-  url = "http://lyricwiki.org/api.php?artist=#{artist}&song=#{params[:song]}&fmt=json"
+  song = params[:song].split().join("%20")
+  url = "http://lyricwiki.org/api.php?artist=#{artist}&song=#{song}&fmt=json"
 
   response = HTTParty.get(url)
 
   content_type :json
 
   return response.to_json
+end
+
+get '/song' do
+
 end
